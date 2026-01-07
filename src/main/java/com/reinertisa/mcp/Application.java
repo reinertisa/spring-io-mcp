12c5b0586a3d7a2e9a387e2ff1f34db6f106d513
Isa Reinert <reinert.isa@gmail.com>
2026-01-07 01:22:42 -0800

    feat(ai): create list callback bean

--- src/main/java/com/reinertisa/mcp/Application.java
+++ src/main/java/com/reinertisa/mcp/Application.java
@@ -1,13 +1,23 @@
 package com.reinertisa.mcp;
 
+import org.springframework.ai.support.ToolCallbacks;
+import org.springframework.ai.tool.ToolCallback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public List<ToolCallback> springSessionTools(SessionTools sessionTools) {
+        return List.of(ToolCallbacks.from(sessionTools));
     }
 
 }
